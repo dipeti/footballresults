3ed7f7de96ea864d3a69528041c9429da9400c5d
Peter Dinya <dinyapeti@gmail.com>
2017-04-09 21:09:22 +0200

    settings activity added

--- app/src/main/java/com/dinya/peter/livefootballresults/MainActivity.java
+++ app/src/main/java/com/dinya/peter/livefootballresults/MainActivity.java
@@ -2,6 +2,7 @@ package com.dinya.peter.livefootballresults;
 
 import android.app.LoaderManager;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -16,6 +17,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
@@ -42,6 +45,7 @@ public class MainActivity extends AppCompatActivity /*implements LoaderManager.L
     Toolbar mToolbar;
     ViewPager mViewPager;
     FragmentPagerAdapter mFragmentPagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,6 +70,23 @@ public class MainActivity extends AppCompatActivity /*implements LoaderManager.L
 //            mDb = dbHelper.getWritableDatabase();
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+           case  R.id.action_settings:
+               Intent startSettingsActivityIntent = new Intent(this,SettingsActivity.class);
+               startActivity(startSettingsActivityIntent);
+               return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private static class MyPagerAdapter extends FragmentPagerAdapter {
         private static final int COUNT = 2;
 
