c7a6da084ac1a2ac942158ae734566aef151212c
Peter Dinya <dinyapeti@gmail.com>
2017-04-13 00:34:28 +0200

    ic_launcher added, item highlighting added

--- app/src/main/java/com/dinya/peter/livefootballresults/sync/BackgroundSyncUtils.java
+++ app/src/main/java/com/dinya/peter/livefootballresults/sync/BackgroundSyncUtils.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 
 import com.dinya.peter.livefootballresults.R;
 import com.dinya.peter.livefootballresults.utils.NetworkUtils;
@@ -27,7 +28,9 @@ public class BackgroundSyncUtils {
             Intent intentToSync = new Intent(context, BackgroundSyncIntentService.class);
             context.startService(intentToSync);
             CoordinatorLayout container = (CoordinatorLayout) ((Activity)context).findViewById(R.id.cl_container);
-            Snackbar.make(container,"Data updated!", Snackbar.LENGTH_SHORT).show();
+            Snackbar snackbar = Snackbar.make(container,"Updating data...", Snackbar.LENGTH_SHORT);
+            snackbar.getView().setBackgroundColor(ContextCompat.getColor(context,R.color.colorPrimary));
+            snackbar.show();
             return true;
         }else {
             CoordinatorLayout container = (CoordinatorLayout) ((Activity)context).findViewById(R.id.cl_container);
