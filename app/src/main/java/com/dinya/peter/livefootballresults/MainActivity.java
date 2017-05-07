263453b6e9875277803982fbfefc7d143cf69a13
Peter Dinya <dinyapeti@gmail.com>
2017-05-07 23:51:16 +0200

    FireBase auth set up

--- app/src/main/java/com/dinya/peter/livefootballresults/MainActivity.java
+++ app/src/main/java/com/dinya/peter/livefootballresults/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -21,6 +22,9 @@ import android.widget.Toast;
 import com.dinya.peter.livefootballresults.database.DbContract;
 import com.dinya.peter.livefootballresults.lists.TableAdapter;
 import com.dinya.peter.livefootballresults.sync.BackgroundSyncUtils;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String INTENT_EXTRA_ID = "id";
 
+    public static final int RC_SIGN_IN = 1; // request code, flag for when we return after startActivityForResult
     private static final String TAG = MainActivity.class.getSimpleName();
 
 
@@ -39,12 +44,15 @@ public class MainActivity extends AppCompatActivity {
     Toolbar mToolbar;
     ViewPager mViewPager;
     FragmentPagerAdapter mFragmentPagerAdapter;
+    FirebaseAuth mFirebaseAuth;
+    FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
         /*
          * Setting up views
          */
@@ -59,8 +67,50 @@ public class MainActivity extends AppCompatActivity {
         /*
          * Start syncing data from remote server at first start.
          */
-        BackgroundSyncUtils.initialize(this);
 
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if  (null == user){
+                    AuthUI authUI = AuthUI.getInstance();
+                    startActivityForResult(
+                            authUI.createSignInIntentBuilder()
+                                    .setProviders(AuthUI.EMAIL_PROVIDER,AuthUI.GOOGLE_PROVIDER)
+                                    .build(),
+                            RC_SIGN_IN);
+                } else {
+                    BackgroundSyncUtils.initialize(MainActivity.this);
+                }
+            }
+        };
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN){
+            if (resultCode==RESULT_OK){
+                FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(MainActivity.this,"Signed-in as " + user.getDisplayName(),Toast.LENGTH_SHORT).show();
+                }
+            } else if(resultCode == RESULT_CANCELED){
+                finish();
+            }
+        }
     }
 
     @Override
@@ -76,6 +126,9 @@ public class MainActivity extends AppCompatActivity {
                Intent startSettingsActivityIntent = new Intent(this,SettingsActivity.class);
                startActivity(startSettingsActivityIntent);
                return true;
+            case R.id.sign_out_menu:
+                AuthUI.getInstance().signOut(this);
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
